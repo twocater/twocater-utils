28df57f9dded253281eaf46ca8f08457d35fe16c
cpaladin <cpaladin@126.com>
2019-01-15 20:52:54 +0800

    test

--- src/main/java/com/twocater/utils/StringUtils.java
+++ src/main/java/com/twocater/utils/StringUtils.java
@@ -1,7 +1,4 @@
 package com.twocater.utils;
 
-/**
- * Created by chenzhiwei on 2016/5/13.
- */
 public class StringUtils {
 }
--- src/main/java/com/twocater/utils/time/DateUtils.java
+++ src/main/java/com/twocater/utils/time/DateUtils.java
@@ -4,13 +4,9 @@ import org.apache.commons.lang3.time.DateFormatUtils;
 
 import java.util.Date;
 
-/**
- * Created by cpaladin on 2016/6/18 15:42.
- * Email: 784615815@qq.com
- */
 public class DateUtils {
 
-    public static String currentTime() {
+    public static String currentDateTime() {
         return DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
     }
 }
